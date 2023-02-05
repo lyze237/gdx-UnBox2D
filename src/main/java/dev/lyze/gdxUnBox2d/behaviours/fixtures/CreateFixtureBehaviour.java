09e16f8a25d5ac07d7753f7c74dc0597a3cc0f23
Captain Lyze <git@lyze.dev>
2023-02-05 17:40:58 +0100

    Implemented box/circle fixture creator behaviours

---
+++ src/main/java/dev/lyze/gdxUnBox2d/behaviours/fixtures/CreateFixtureBehaviour.java
@@ -0,0 +1,43 @@
+package dev.lyze.gdxUnBox2d.behaviours.fixtures;
+
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Shape;
+import dev.lyze.gdxUnBox2d.GameObject;
+import dev.lyze.gdxUnBox2d.behaviours.BehaviourAdapter;
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * Creates a box fixture in awake.
+ */
+public abstract class CreateFixtureBehaviour extends BehaviourAdapter {
+    @Getter private Fixture fixture;
+    @Getter(AccessLevel.PROTECTED) private final FixtureDef fixtureDef;
+
+    /**
+     * Creates a fixture in awake.
+     * @param gameObject The game object to attach to.
+     */
+    public CreateFixtureBehaviour(GameObject gameObject) {
+        this(new FixtureDef(), gameObject);
+    }
+
+    /**
+     * Creates a fixture in awake.
+     * @param fixtureDef The fixture definition to use as template.
+     * @param gameObject The game object to attach to.
+     */
+    public CreateFixtureBehaviour(FixtureDef fixtureDef, GameObject gameObject) {
+        super(gameObject);
+
+        this.fixtureDef = fixtureDef;
+    }
+
+    protected void createAndAttachFixture(Shape shape) {
+        fixtureDef.shape = shape;
+
+        fixture = getGameObject().getBody().createFixture(fixtureDef);
+        shape.dispose();
+    }
+}
