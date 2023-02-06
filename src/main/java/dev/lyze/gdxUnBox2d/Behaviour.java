84ad5b040192f68e01b67e0d20a5a1a3f6c3ca6d
Lyze <11274700+lyze237@users.noreply.github.com>
2023-02-06 07:39:09 +0000

    Added helper getUnBox method to behaviour

--- src/main/java/dev/lyze/gdxUnBox2d/Behaviour.java
+++ src/main/java/dev/lyze/gdxUnBox2d/Behaviour.java
@@ -112,4 +112,8 @@ public abstract class Behaviour {
     public void destroy() {
         getGameObject().getUnBox().destroy(this);
     }
+
+    public UnBox getUnBox() {
+        return getGameObject().getUnBox();
+    }
 }
