a91ff81fa352626253548cc9f91e5cf99291324b
Captain Lyze <git@lyze.dev>
2023-03-29 12:29:21 +0200

    Update PhysicsOptions.java

--- src/main/java/dev/lyze/gdxUnBox2d/options/PhysicsOptions.java
+++ src/main/java/dev/lyze/gdxUnBox2d/options/PhysicsOptions.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class PhysicsOptions {
-    @Getter @Setter private float timeStep = 1 / 30f;
+    @Getter @Setter private float timeStep = 1 / 60f;
     @Getter @Setter private float maxFixedFrameTime = 0.25f;
 }
