d3e34d3867f66ef56d1304cf8db181946dae32ac
Lyze <11274700+lyze237@users.noreply.github.com>
2023-05-16 09:45:39 +0200

    Shannon rule
    https://gamedev.stackexchange.com/a/150814

--- src/main/java/dev/lyze/gdxUnBox2d/options/PhysicsOptions.java
+++ src/main/java/dev/lyze/gdxUnBox2d/options/PhysicsOptions.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class PhysicsOptions {
-    @Getter @Setter private float timeStep = 1 / 60f;
+    @Getter @Setter private float timeStep = 1 / 120f;
     @Getter @Setter private float maxFixedFrameTime = 0.25f;
     @Getter @Setter private boolean interpolateMovement = true;
 }
